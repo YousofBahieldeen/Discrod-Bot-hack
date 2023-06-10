29d4a4849f98f7077c088516fe3738734fd01974
YousofBahieldeen <108595154+YousofBahieldeen@users.noreply.github.com>
2023-06-10 12:43:59 -0400

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,4 +1,15 @@
-Import Os
+import os
 
+import discord
+from dotenv import load_dotenv
 
-print("Hello World!")
+load_dotenv()
+TOKEN = os.getenv('MTExNzEyNDA4NTU1MDQyODIyMA.Gvg3JH.6SP_nwCVx1wPT8SNZV-LUPxmdv5LNhYgDjZ-gs')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
